5a2e1fe62fde889492191a1237f9ec00f97a72f6
mambli-joe <anton.mambli@gmail.com>
2024-11-16 18:03:10 +0300

    домашка по react compound

---
+++ src/components/Compound/CompoundList.tsx
@@ -0,0 +1,116 @@
+import React, { ReactNode, useState } from 'react'
+import { noop } from 'lodash'
+
+type Option = { value: string; label: string }
+
+type CompoundListContextProps = {
+    children: ReactNode
+}
+
+type CompoundListContext = {
+    selectedOptions: string[]
+    onChange: (value: string) => void
+    clear: () => void
+}
+
+type CompoundListProps = {
+    children: ReactNode
+}
+
+type CompoundListItemProps = {
+    option: Option
+}
+
+const CompoundListContext = React.createContext<CompoundListContext>({
+    selectedOptions: [],
+    onChange: noop,
+    clear: noop,
+})
+
+function CompoundList({ children }: CompoundListContextProps) {
+    const [selectedOptions, setSelectedOptions] = useState<string[]>([])
+
+    const onChange = (value: string) => {
+        const alreadySelected = selectedOptions.includes(value)
+        const newSelectedOptions = alreadySelected
+            ? selectedOptions.filter((item) => item !== value)
+            : [...selectedOptions, value]
+        console.log({ newSelectedOptions, alreadySelected, value })
+        setSelectedOptions(newSelectedOptions)
+    }
+
+    const clear = () => {
+        setSelectedOptions([])
+    }
+
+    const value = React.useMemo(
+        () => ({
+            selectedOptions,
+            onChange,
+            clear,
+        }),
+        [selectedOptions]
+    )
+
+    return (
+        <CompoundListContext.Provider value={value}>
+            {children}
+        </CompoundListContext.Provider>
+    )
+}
+
+function useCompoundList(): CompoundListContext {
+    const context = React.useContext(CompoundListContext)
+
+    if (!context) {
+        throw new Error(
+            'This component must be used within a <CompundListProvider> component.'
+        )
+    }
+
+    return context
+}
+
+CompoundList.List = function List({ children }: CompoundListProps) {
+    const { selectedOptions, clear } = useCompoundList()
+    return (
+        <div className="flex flex-col gap-4 align-middle">
+            <div className="">
+                Выбраны:{' '}
+                {selectedOptions.map((item) => (
+                    <span>{item}</span>
+                ))}
+            </div>
+            <div className="">Выбрано: {selectedOptions.length}</div>
+            <button
+                type="button"
+                className="py-2.5 px-6 text-sm bg-indigo-500 text-white rounded-full cursor-pointer font-semibold text-center shadow-xs transition-all duration-500 hover:bg-indigo-700"
+                onClick={clear}
+            >
+                Очистить
+            </button>
+            <div className="mt-2">{children}</div>
+        </div>
+    )
+}
+CompoundList.Item = function Checkbox({ option }: CompoundListItemProps) {
+    const { selectedOptions, onChange } = useCompoundList()
+    return (
+        <div className="flex items-center  mb-4">
+            <label
+                className="text-sm font-norma cursor-pointer text-gray-600 flex align-baseline"
+                onChange={(_) => onChange(option.value)}
+            >
+                <input
+                    checked={selectedOptions.includes(option.value)}
+                    type="checkbox"
+                    value={option.value}
+                    className="w-5 h-5 appearance-none border cursor-pointer border-gray-300  rounded-md mr-2 hover:border-indigo-500 hover:bg-indigo-100 checked:bg-no-repeat checked:bg-center checked:border-indigo-500 checked:bg-indigo-100"
+                />
+                {option.value}
+            </label>
+        </div>
+    )
+}
+
+export default CompoundList
--- src/components/Compound/index.tsx
+++ src/components/Compound/index.tsx
@@ -1,11 +1,46 @@
 import React from 'react'
-import Navigation from '../Navigation'
 import Layout from '../Layout'
+import CompoundList from './CompoundList.tsx'
+import { range } from 'lodash'
+import { faker } from '@faker-js/faker/locale/en'
+
+type Option = { value: string; label: string }
+
+const options: Option[] = range(5).map((_item) => ({
+    value: faker.string.uuid(),
+    label: faker.lorem.lines(),
+}))
 
 const Compound = () => {
     return (
         <Layout title={'React: Compound'}>
-            <div>Template</div>
+            <h2 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black">
+                Практика:
+                <mark className="px-2 ml-2 text-white bg-blue-600 rounded dark:bg-blue-500">
+                    Список
+                </mark>{' '}
+            </h2>
+            <CompoundList>
+                <CompoundList.List>
+                    {options.map((option) => (
+                        <CompoundList.Item option={option} />
+                    ))}
+                </CompoundList.List>
+            </CompoundList>
+            <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+            <h2 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black">
+                Практика:
+                <mark className="px-2 ml-2 text-white bg-blue-600 rounded dark:bg-blue-500">
+                    Карточка
+                </mark>{' '}
+            </h2>
+            <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+            <h2 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black">
+                Практика:
+                <mark className="px-2 ml-2 text-white bg-blue-600 rounded dark:bg-blue-500">
+                    Табы
+                </mark>{' '}
+            </h2>
         </Layout>
     )
 }
